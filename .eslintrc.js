bc305a5effc0f3d9e87323ccec72b68021a5e9a8
willy <willy8742891@gmail.com>
2022-03-10 22:46:22 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+};
