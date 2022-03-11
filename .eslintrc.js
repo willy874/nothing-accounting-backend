cb77bcd529f8403485d204ee2ed779ea576c1480
katsudevelopers <katsudevelopers@gmail.com>
2022-03-11 14:08:57 +0800

    2nd commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,15 @@
 module.exports = {
   env: {
     node: true,
-    commonjs: true,
+    // commonjs: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-    'prettier',
-  ],
+  extends: ["airbnb-base", "prettier"],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: "latest",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
 };
